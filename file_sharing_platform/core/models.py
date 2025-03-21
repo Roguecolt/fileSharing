23107b2473717a88bd48752f0a5d43a8af55e1eb
Aravind <aravindvinod050@gmail.com>
2025-03-21 15:42:21 +0400

    Added file preview with modals

--- file_sharing_platform/core/models.py
+++ file_sharing_platform/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import os
 # Create your models here.
 class File(models.Model):
     name=models.CharField(max_length=255)
@@ -8,8 +8,11 @@ class File(models.Model):
     upload_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_file_type(self):
+        return os.path.splitext(self.uploaded_file.name)[1].lower()
+
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.get_file_type()})"
     
 class SharedFile(models.Model):
     file = models.ForeignKey(File, on_delete=models.CASCADE)

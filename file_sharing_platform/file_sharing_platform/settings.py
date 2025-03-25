aa8e3682c777f9bc69b81101d069a93071cf48db
Aravind <aravindvinod050@gmail.com>
2025-03-25 09:47:37 +0400

    Dockerized the project

--- file_sharing_platform/file_sharing_platform/settings.py
+++ file_sharing_platform/file_sharing_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^=m@a=ie*(rj2p-@ie)&yeag&=1fl*22+5*%x%hmxf_3-m3=e(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
